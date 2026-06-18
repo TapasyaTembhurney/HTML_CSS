186bd2d2b9f5828171f64431d68a4d328c06437d
Your name <tapasyatembhurney@gmail.com>
2026-06-18 19:45:00 +0530

    if-else-elseif statement

---
+++ JAVASCRIPT/DAY 1/DAY 2/example.js
@@ -0,0 +1,16 @@
+let userRole = 'admin';
+
+if(userRole == 'admin'){
+    console.log("Full Access granted");
+}
+
+else if(userRole == "editor"){
+    console.log("editor access granted");
+}
+
+else if(userRole == "author"){
+    console.log("author access granted");
+}
+else{
+    console.log("Student Access Granted");
+}
