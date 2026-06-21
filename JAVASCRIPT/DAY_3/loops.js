42aa78c45bbbfb154f3edb0c67d444a3eef0d670
Your name <tapasyatembhurney@gmail.com>
2026-06-21 18:41:54 +0530

    loops

---
+++ JAVASCRIPT/DAY_3/loops.js
@@ -0,0 +1,31 @@
+// FOR LOOP
+let i;
+
+for(i = 1 ; i <=5 ; i++){
+    console.log("*");
+}  
+
+console.log("\n");
+
+// WHILE LOOP
+
+let j = 1;
+
+while(j <= 5){
+    console.log("*");
+    j = j + 1;
+}
+
+console.log("\n");
+
+// DO-WHILE LOOP
+let k = 1;
+let result = "";
+do{
+    result += "*";
+    k++;
+}
+while(k <= 10);
+
+console.log(result);
+
